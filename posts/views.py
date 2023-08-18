77d5c39920ee90c197a082a85e2b93a02ab87aec
Goldenmarquice <usmcmarquice@gmail.com>
2023-08-17 21:02:03 -0700

    Add existing file

---
+++ posts/views.py
@@ -0,0 +1,45 @@
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+
+from .models import Post
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import(
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    feilds = ["title", "subtitle", "author", "body"]
+
+class PostUpdatetView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    feilds = ["title", "subtitle", "body"]
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("list")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
